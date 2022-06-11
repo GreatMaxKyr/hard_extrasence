c4d3dc340f4b95229eb21dd356840b948d21d80f
Great_MaxKyr <maxkyrylyuk@gmail.com>
2022-06-11 09:55:14 +0300

    new viewport

--- end.php
+++ end.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="css/style.css">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=yes , initial-scale=1.0 , minimum-scalable=1.0 , maximum-scale=2.0">
     <title>Document</title>
 </head>
 <body class=''>
